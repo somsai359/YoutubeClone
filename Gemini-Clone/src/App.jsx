5cb73df7c4288fbfb0d11a67890b3dd11cd53036
somsai359 <somasaichowdary01@gmail.com>
2025-05-14 10:10:12 +0530

    Ui

---
+++ Gemini-Clone/src/App.jsx
@@ -0,0 +1,15 @@
+import React from  'react'
+import SideBar from './components/Sidebar/Sidebar'
+import Main from './components/Main/Main'
+import Gemini from './config/gemini'
+const App = () => {
+
+  return (
+    <>
+      <SideBar/>
+      <Main/>
+    </>
+  )
+}
+
+export default App
